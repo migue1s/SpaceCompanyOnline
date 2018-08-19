225ed7698af95a51559d8b03cb1dd1741e15dabd
Miguel Saiz <miguelsaizo@gmail.com>
2018-08-19 05:41:20 -0400

    Adds login method

--- src/user.js
+++ src/user.js
@@ -1,14 +1,30 @@
 import bcrypt from 'bcrypt';
 
 export const register = async (event, context, callback) => {
-  const payload = JSON.parse(event.body);
+  const body = JSON.parse(event.body);
 
-  const hash = await bcrypt.hash(payload.password, 10);
-  console.log(hash);
+  const hash = await bcrypt.hash(body.password, 10);
+  const user = {
+    username: body.username,
+    password: hash,
+  };
 
-  callback(null, payload);
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(user),
+  };
+
+  callback(null, response);
 };
 
 export const login = async (event, context, callback) => {
+  const body = JSON.parse(event.body);
+  const didAuthorize = await bcrypt.compare(body.password, "$2b$10$/KKxG72w0GMu/wHAGn/Iuea8BBM2wBJEQxgqiiYqZhv0XtWFglYku");
 
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ didAuthorize }),
+  };
+
+  callback(null, response);
 };
