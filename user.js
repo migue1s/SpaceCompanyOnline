85f04132c0a665dd6987a654c1eed788a50d7c81
Miguel Saiz <miguelsaizo@gmail.com>
2018-08-19 16:20:29 -0400

    Adds login

--- user.js
+++ user.js
@@ -4,12 +4,14 @@ import { success, failure } from './responseBuilder';
 
 export async function create(event, context, callback) {
   const data = JSON.parse(event.body);
-  const hash = await bcryptjs.hash(data.username, 10);
+  const salt = await bcryptjs.genSalt();
+  const hash = await bcryptjs.hash(data.password, salt);
   const params = {
     TableName: process.env.tableName,
     Item: {
       username: data.username,
       password: hash,
+      passwordSalt: salt,
       createdAt: Date.now()
     },
     ConditionExpression: 'username <> :username',
@@ -21,6 +23,27 @@ export async function create(event, context, callback) {
   try {
     await call("put", params);
     callback(null, success(params.Item));
+  } catch (e) {
+    callback(null, failure(e));
+  }
+}
+
+export async function login(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.tableName,
+    Key: {
+      username: data.username,
+    },
+  };
+  try {
+    const result = await call("get", params);
+    const user = result.Item;
+    if (await bcryptjs.compare(data.password, user.password)) {
+      callback(null, success(user));
+    } else {
+      callback(null, failure({ message: 'Invalid password.', user }));
+    }
   } catch (e) {
     callback(null, failure(e));
   }
