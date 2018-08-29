29645911d3c70ebae420e25c28a61e6fe3020d03
Miguel Saiz <miguelsaizo@gmail.com>
2018-08-28 20:26:27 -0400

    Adds JWT-based authorization

--- jwtAuthenticator.js
+++ jwtAuthenticator.js
@@ -1,5 +1,21 @@
+import jwt from 'jsonwebtoken';
+import policyBuilder from './utils/policyBuilder';
+import config from './config';
 
+export async function jwtAuthorizer(event, context, callback) {
 
-export function jwtAuthorizer(event, context, callback) {
+  const token = event.authorizationToken;
 
+  try {
+    const decoded = await jwt.verify(token, config.jwtSecret);
+    const username = decoded.username;
+
+    const authorizerContext = { username };
+    // Return an IAM policy document for the current endpoint
+    const policyDocument = policyBuilder(username, 'Allow', event.methodArn, authorizerContext);
+
+    callback(null, policyDocument);
+  } catch (e) {
+    callback(event); // Return a 401 Unauthorized response
+  }
 }
---
+++ utils/policyBuilder.js
@@ -0,0 +1,18 @@
+export default function(userId, effect, resource, context) {
+  const policy = {
+    principalId: userId,
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: effect,
+          Resource: resource,
+        },
+      ],
+    },
+    context,
+  };
+
+  return policy;
+};
